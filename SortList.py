937d831a47bd93185b7b92969a9574dd16ff0a81
kiranHR <kiranhr52@gmail.com>
2021-01-13 02:09:19 +0530

    Update SortList.py

--- SortList.py
+++ SortList.py
@@ -1,15 +1,14 @@
+def sort(lst): 
+    lst = [str(i) for i in lst] 
+    lst.sort() 
+    lst = [int(i) if i.isdigit() else i for i in lst ] 
+    return lst 
+              
+# Driver code
 NumList = []
-
 Number = int(input("Please enter the Total Number of List Elements: "))
 for i in range(1, Number + 1):
-    value = int(input("Please enter the Value of %d Element : " %i))
+    value = input("Please enter the Value of %d Element : " %i)
     NumList.append(value)
-
-for i in range (Number):
-    for j in range(i + 1, Number):
-        if(NumList[i] > NumList[j]):
-            temp = NumList[i]
-            NumList[i] = NumList[j]
-            NumList[j] = temp
-
-print("Element After Sorting List in Ascending Order is : ", NumList)
+#lst = ['k', 5, 'e', 3, 'g', 7, 0, 't'] 
+print(sort(NumList)) 
